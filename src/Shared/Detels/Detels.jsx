f90fb40f22c42096fa7529932d9df7d601f95f1a
Tonmoy-Rahman-Turjo <tonmoy9828@gmai.com>
2024-06-22 01:17:32 +0600

    rating add

--- src/Shared/Detels/Detels.jsx
+++ src/Shared/Detels/Detels.jsx
@@ -1,4 +1,4 @@
-import { toast } from "react-toastify";
+import { ToastContainer, toast } from "react-toastify";
 import Loader from "../../Route/Loader";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import UseAuth from "../../UseHook/UseAuth";
@@ -27,7 +27,7 @@ const Detels = () => {
       try {
         await voteIncrement();
         refetch();
-        toast("Woww! Vote done", {
+        toast("successfully count your vote", {
           icon: "👏",
         });
       } catch (error) {
@@ -59,76 +59,153 @@ const Detels = () => {
   
     const { productsName,productsImg, description, tags, externalLinks, upVote, _id, ProductFeedback, voters, email, } = data.data;
     return (
-       <div>
-         <div className="relative flex bg-clip-border rounded-xl bg-white text-gray-700 shadow-lg w-11/12 mx-auto md:flex-row flex-col">
-        <div className="relative md:w-2/5 m-0 overflow-hidden text-gray-700 bg-white rounded-r-none bg-clip-border rounded-xl shrink-0">
-          <img src={productsImg} className="object-cover w-full h-full" />
-        </div>
-        <div className="p-6">
-          <h6 className="block mb-4 font-sans text-base antialiased font-semibold leading-relaxed tracking-normal text-gray-700">
-            {Array.isArray(tags) &&
-              tags?.map((tag, index) => (
-                <span
-                  key={index}
-                  className="mr-2 mb-2 bg-green-100 text-green-600 py-1 px-2 rounded-full text-sm font-semibold"
-                >
-                  {tag}
-                </span>
-              ))}
-          </h6>
-          <h4 className="block mb-6 mt-6 font-sans text-2xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
-            {productsName}
-          </h4>
-          <p className="block mb-5 font-sans text-base antialiased font-normal leading-relaxed text-gray-700">
-            {description}
-          </p>
-          <a
-            href={
-                externalLinks}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="block text-xl antialiased font-medium text-blue-700 hover:underline leading-snug tracking-normal text-blue-gray-900"
-          >
-            {externalLinks}
-          </a>
-          <div className="flex gap-6 justify-end">
-            <button
-              onClick={() => {
-                if (voters?.includes(user?.email)) {
-                  toast.error("You've already voted this product");
-                } else {
-                  handleVoteCount();
-                }
-              }}
-              disabled={user?.email === email}
-              className={`py-1 px-4 hover:text-green-600 hover:scale-105 hover:shadow text-center border rounded-md border-gray-300 h-8 text-sm flex items-center gap-1 lg:gap-2 ${
-                user?.email === email
-                  ? "cursor-not-allowed opacity-60 hover:text-black"
-                  : ""
-              }`}
-            >
-              <BiUpvote className="text-2xl"></BiUpvote>
-              <span className="text-lg">{upVote}</span>
-            </button>
-            <button className="py-1 px-4 hover:text-red-600 hover:scale-105 hover:shadow text-center border border-gray-300 rounded-md h-8 text-sm flex items-center gap-1 lg:gap-2">
-              <BiDownvote className="hover:text-red-600 text-2xl"></BiDownvote>
-              <span className="text-lg">0</span>
-            </button>
+        <div className="bg-[#4aaad6] py-10">
+          <div className="flex rounded-xl md:w-[700px] w-full gap-2 m-auto  bg-white  shadow-lg mx-auto md:flex-row flex-col">
+          <img src={productsImg} className="rounded-l-xl " />
+          <div>
+
+          </div>
+
+
+          <div>
+   <div className="flex mt-3  items-center justify-between">
+   <h6 className="block mb-4 font-sans text-base antialiased font-semibold leading-relaxed tracking-normal text-gray-700">
+   {Array.isArray(tags) &&
+     tags?.map((tag, index) => (
+       <span
+         key={index}
+         className="mr-2 mb-2 bg-green-100 text-green-600 py-1 px-2 rounded-full text-sm font-semibold"
+       >
+         {tag}
+       </span>
+     ))}
+ </h6>
+  <button
+   onClick={() => handleReportedProduct(_id)}
+   className="text-white bg-blue-700 hover:bg-blue-800  font-medium rounded-lg text-xl px-6 italic py-1.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+ >
+   {ProductFeedback === "Reported" ? "Reported" : "Report"}
+  </button>
+   </div>
+ <h4 className="block mb-6 mt-6 font-sans text-2xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+    {productsName}
+  </h4>
+  <p className="block mb-3 font-sans text-base antialiased font-normal leading-relaxed text-gray-700">
+   {description}
+  </p>
+  <a
+   href={
+       externalLinks}
+   target="_blank"
+   rel="noopener noreferrer"
+   className="block  antialiased font-medium text-blue-700 hover:underline leading-snug tracking-normal text-blue-gray-900"
+ >
+   {externalLinks}
+ </a>
+ <div className="mt-3">
+   <button
+     onClick={() => {
+       if (voters?.includes(user?.email)) {
+         toast.error("You've already voted this product");
+       } else {
+         handleVoteCount();
+       }
+     }}
+     disabled={user?.email === email}
+     className={`py-1 px-4 hover:text-black mb-5 text-white bg-green-700 hover:scale-105 hover:shadow text-center border rounded-md border-gray-300 h-8 text-sm flex items-center gap-1 lg:gap-2 ${
+       user?.email === email
+         ? "cursor-not-allowed opacity-60 hover:text-black"
+         : ""
+     }`}
+   >
+     <BiUpvote className="text-2xl"></BiUpvote>
+      <span className="text-lg">{upVote}</span>
+   </button>
+  
+ </div>
+            
+          </div>
           </div>
         </div>
-        <div>
-          <button
-            onClick={() => handleReportedProduct(_id)}
-            className="text-white bg-blue-700 hover:bg-blue-800  font-medium rounded-lg text-xl px-6 italic py-1.5 m-4 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            {ProductFeedback === "Reported" ? "Reported" : "Report"}
-          </button>
-        </div>
-      </div>
-      {/* {/* <PostReview productId={_id} /> */}
-      {/* <Allrivew productId={_id} />  */}
-       </div>
     );
 };
+
+
+
+
+
+
+
+
+
+
+
+// <div className=" bg-[#4aaad6] py-10">
+// <div className="flex rounded-xl w-7/12  m-auto  bg-white  shadow-lg mx-auto md:flex-row flex-col">
+// <div className="">
+//  <img src={productsImg} className="object-cover rounded-l-xl w-full h-full" />
+// </div>
+// <div className="p-6">
+//  <h6 className="block mb-4 font-sans text-base antialiased font-semibold leading-relaxed tracking-normal text-gray-700">
+//    {Array.isArray(tags) &&
+//      tags?.map((tag, index) => (
+//        <span
+//          key={index}
+//          className="mr-2 mb-2 bg-green-100 text-green-600 py-1 px-2 rounded-full text-sm font-semibold"
+//        >
+//          {tag}
+//        </span>
+//      ))}
+//  </h6>
+//  <h4 className="block mb-6 mt-6 font-sans text-2xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+//    {productsName}
+//  </h4>
+//  <p className="block mb-3 font-sans text-base antialiased font-normal leading-relaxed text-gray-700">
+//    {description}
+//  </p>
+//  <a
+//    href={
+//        externalLinks}
+//    target="_blank"
+//    rel="noopener noreferrer"
+//    className="block  antialiased font-medium text-blue-700 hover:underline leading-snug tracking-normal text-blue-gray-900"
+//  >
+//    {externalLinks}
+//  </a>
+//  <div className="mt-3">
+//    <button
+//      onClick={() => {
+//        if (voters?.includes(user?.email)) {
+//          toast.error("You've already voted this product");
+//        } else {
+//          handleVoteCount();
+//        }
+//      }}
+//      disabled={user?.email === email}
+//      className={`py-1 px-4 hover:text-black text-white bg-green-700 hover:scale-105 hover:shadow text-center border rounded-md border-gray-300 h-8 text-sm flex items-center gap-1 lg:gap-2 ${
+//        user?.email === email
+//          ? "cursor-not-allowed opacity-60 hover:text-black"
+//          : ""
+//      }`}
+//    >
+//      <BiUpvote className="text-2xl"></BiUpvote>
+//      <span className="text-lg">{upVote}</span>
+//    </button>
+  
+//  </div>
+// </div>
+// <div>
+// <button
+//    onClick={() => handleReportedProduct(_id)}
+//    className="text-white bg-blue-700 hover:bg-blue-800  font-medium rounded-lg text-xl px-6 italic py-1.5 m-4 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+//  >
+//    {ProductFeedback === "Reported" ? "Reported" : "Report"}
+//  </button>
+// </div>
+// </div>
+// <ToastContainer/>
+// {/* {/* <PostReview productId={_id} /> */}
+// {/* <Allrivew productId={_id} />  */}
+// </div>
 
 export default Detels;
---
+++ src/Shared/PostRivew.jsx
@@ -0,0 +1,112 @@
+import { useState } from "react";
+import UseAuth from "../UseHook/UseAuth";
+// import UseAxios from "../UseHook/UseAxios";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import UseAxios from "../UseHook/UseAxios";
+import { toast } from "react-toastify";
+
+
+const PostRivew = ({ productId }) => {
+    const { user } = UseAuth();
+    const { displayName, photoURL } = user;
+    const axiosSecure = UseAxios();
+    const [rating, setRating] = useState(0);
+    const queryClient = useQueryClient();
+  
+    const { mutateAsync: addReview } = useMutation({
+      mutationFn: async (usersReview) =>
+        await axiosSecure.post("/addReview", usersReview),
+      onSuccess: () => {
+        queryClient.invalidateQueries("allReviews");
+      },
+    });
+  
+    const handleAddProduct = async (event) => {
+      event.preventDefault();
+  
+      const form = event.target;
+      const feedback = form.feedback.value;
+  
+      const usersReview = {
+        displayName,
+        photoURL,
+        feedback,
+        rating,
+        productId,
+      };
+  
+      try {
+        await addReview(usersReview);
+        form.reset();
+        setRating(0);
+        toast.success("Thanks for your Feedback");
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    return (
+        <div>
+             <form
+      onSubmit={handleAddProduct}
+      className="w-7/12 mx-auto mt-20 p-6 bg-white border rounded-lg shadow-lg"
+    >
+      <h2 className="text-2xl font-bold mb-6">Feedback Form</h2>
+      <div className="mb-8">
+        <label className="block text-gray-700 font-bold mb-3" htmlFor="name">
+          Name :
+        </label>
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-800 font-semibold italic text-xl leading-tight focus:outline-none focus:shadow-outline"
+          id="name"
+          type="text"
+          name="name"
+          placeholder="Enter your name"
+          defaultValue={displayName}
+          readOnly
+        />
+      </div>
+      <div className="">
+        <label className="block text-gray-700 font-bold mb-3" htmlFor="photo">
+          PhotoURL :
+        </label>
+        <input
+          className="shadow text-base appearance-none border rounded w-full py-2 px-3 text-gray-900 font-medium leading-tight focus:outline-none focus:shadow-outline"
+          id="photo"
+          type="text"
+          name="photo"
+          placeholder="Your photoURL"
+          defaultValue={photoURL}
+          readOnly
+        />
+      </div>
+      <div className="mb-6 mt-6">
+        <Rating value={rating} onChange={setRating} style={{ maxWidth: 230 }} />
+      </div>
+      <div className="mb-4">
+        <label
+          className="block mt-4 text-gray-700 font-bold mb-3"
+          htmlFor="feedback"
+        >
+          Your Feedback :
+        </label>
+        <textarea
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="feedback"
+          name="feedback"
+          rows={5}
+          placeholder="Enter your feedback"
+        />
+      </div>
+
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        type="submit"
+      >
+        Submit
+      </button>
+    </form>
+        </div>
+    );
+};
+
+export default PostRivew;
